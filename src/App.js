2207a675122c8b598e9de48b27001a4945ddc8c9
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-22 11:32:01 +0530

    30.2 redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
 import Users from './user/pages/Users'
 
 const App = () => {
-  return <Router>
-    <Route path="/"> //this '/' will render '/'' and anything after is as well for example '/dshf' will also fetch same data as '/'
+  return (
+  <Router>
+    <Route path="/" exact> //'exact will ensure that only '/' works and anything beyond that slash will not fetch the same result as '/'
       <Users />
     </Route>
-  </Router>;
+    <Redirect to ="/" /> This redirect will redirect the unmade links to the specified page which is '/'
+  </Router>
+  );
 };
 
 export default App;
