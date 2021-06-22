cd8aca989dd864925164773960d49dc4357dd404
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-22 10:42:54 +0530

    38.1 '/'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+import Users from './user/pages/Users'
+
+const App = () => {
+  return <Router>
+    <Route path="/"> //this '/' will render '/'' and anything after is as well for example '/dshf' will also fetch same data as '/'
+      <Users />
+    </Route>
+  </Router>;
+};
 
 export default App;
