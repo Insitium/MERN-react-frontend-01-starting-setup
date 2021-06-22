dcbb39e4a431c1f42141b0c5663827cb5ea4d8d0
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-22 16:33:20 +0530

    39  Adding a UsersList Page / Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users =() => {
-    return <h2>
-        Users Works!
-    </h2>
+    const USERS = [{id:'u1',name:'Sarthak Vashistha', image:'https://media-exp3.licdn.com/dms/image/C5103AQFeGcRbDhXpIg/profile-displayphoto-shrink_200_200/0/1516713032582?e=1629936000&v=beta&t=PDgl45aArP5jQ6pKJBk6WADUyxcxSevAQBNa8PYQF2g',places: 3
+}
+];
+
+    return <UsersList items ={USERS} />;
 }
 
 export default Users;
