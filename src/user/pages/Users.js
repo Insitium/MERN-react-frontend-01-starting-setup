0fe43e809e4bd50a594d3e7bc3775423cbca0984
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-27 15:39:57 +0530

    41 styling our App and more components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,7 +3,7 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users =() => {
-    const USERS = [{id:'u1',name:'Sarthak Vashistha', image:'https://media-exp3.licdn.com/dms/image/C5103AQFeGcRbDhXpIg/profile-displayphoto-shrink_200_200/0/1516713032582?e=1629936000&v=beta&t=PDgl45aArP5jQ6pKJBk6WADUyxcxSevAQBNa8PYQF2g',places: 3
+    const USERS = [{id:'9990579707',name:'Sarthak Vashistha', image:'https://media-exp3.licdn.com/dms/image/C5103AQFeGcRbDhXpIg/profile-displayphoto-shrink_200_200/0/1516713032582?e=1629936000&v=beta&t=PDgl45aArP5jQ6pKJBk6WADUyxcxSevAQBNa8PYQF2g',places: 3
 }
 ];
 
