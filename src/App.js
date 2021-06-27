268988d9ecbc9b00bd7a43c81c21e5a33c66f21e
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-06-27 16:05:03 +0530

    40 with correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import { BrowserRouter as Router,
+   Route,
+    Redirect,
+     Switch}
+      from 'react-router-dom';
 
 
 import Users from './user/pages/Users'
--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import './UserItem.css';
-import Avatar from '../../shared/components/UIElements/Avatar';
 
 const UserItem = props => {
-    return (
-        <li classname ="user-item">
-            <div className = "user-item__content">
-                <link to= {`/${props.id}/places`}>
-                <div className = "user-item__image"></div>
-                <Avatar image={props.image} alt = {props.name} />
-                <div className="user-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount ===1 ? 'place' : 'places'}
-                    </h3>
-                    <h4>{props.id}</h4>
-                </div>
-                </link>
-            </div>
-        </li>
-    );
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
