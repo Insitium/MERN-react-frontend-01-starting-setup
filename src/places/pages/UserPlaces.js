671a93f5f6daef3733cc464bf8ef343a1c76f15f
Sarthak Vashistha <vashistha.sarthak31@gmail.com>
2021-07-04 10:59:18 +0530

    49.2 Rendering User Places & Using Dynamic Route Segments

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -18,13 +18,13 @@ const DUMMY_PLACES =[
     
     {
         id:'p2',
-        title:'empire satte building',
-        description:'One of the most famous sky scrappers in the world!',
-        imageUrl:'https://static.toiimg.com/thumb/71579199/empire-state-building.jpg?width=1200&height=900',
-        address: '20 W 34th St, New York, NY 10001, United States',
+        title:'Seneca College',
+        description:'My grad School',
+        imageUrl:'https://www.senecacollege.ca/content/dam/projects/seneca/campus-photos/seneca-at-york-entrance.jpg',
+        address: '1750 Finch Ave E, North York, ON M2J 2X5, Canada',
         location:{
-            lat:40.7484564 ,
-            long:-73.9944192
+            lat:43.7961723,
+            long:-79.3572865
         },
         creater:'u1'
     }
